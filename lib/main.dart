476677bf4696d26451271e476f0c980a4db09950
Dino Hurem <dino.hurem@authoritypartners.com>
2022-03-25 17:07:14 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,269 @@
+library event_calendar;
+
+import 'dart:math';
+import 'package:day_night_time_picker/day_night_time_picker.dart';
+import 'package:day_night_time_picker/lib/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+part 'screens/color_picker.dart';
+part 'screens/appointment_editor.dart';
+
+void main() => runApp(
+      const MaterialApp(
+        home: EventCalendar(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+//ignore: must_be_immutable
+class EventCalendar extends StatefulWidget {
+  const EventCalendar({Key? key}) : super(key: key);
+
+  @override
+  EventCalendarState createState() => EventCalendarState();
+}
+
+List<Color> _colorCollection = <Color>[];
+List<String> _colorNames = <String>[];
+int _selectedColorIndex = 0;
+List<String> _timeZoneCollection = <String>[];
+late DataSource _events;
+Meeting? _selectedAppointment;
+late DateTime _startDate;
+late TimeOfDay _startTime;
+late DateTime _endDate;
+late TimeOfDay _endTime;
+bool _isAllDay = false;
+String _subject = '';
+String _notes = '';
+
+class EventCalendarState extends State<EventCalendar> {
+  EventCalendarState();
+
+  CalendarView _calendarView = CalendarView.month;
+  late List<String> eventNameCollection;
+  late List<Meeting> appointments;
+
+  @override
+  void initState() {
+    _calendarView = CalendarView.month;
+    appointments = getMeetingDetails();
+    _events = DataSource(appointments);
+    _selectedAppointment = null;
+    _selectedColorIndex = 0;
+    _subject = '';
+    _notes = '';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+            child: getEventCalendar(_calendarView, _events, onCalendarTapped)));
+  }
+
+  SfCalendar getEventCalendar(
+      CalendarView _calendarView,
+      CalendarDataSource _calendarDataSource,
+      CalendarTapCallback calendarTapCallback) {
+    return SfCalendar(
+        headerDateFormat: 'MMM,yyy',
+        view: _calendarView,
+        dataSource: _calendarDataSource,
+        onTap: calendarTapCallback,
+        initialDisplayDate: DateTime(DateTime.now().year, DateTime.now().month,
+            DateTime.now().day, 0, 0, 0),
+        monthViewSettings: const MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        timeSlotViewSettings: const TimeSlotViewSettings(
+            minimumAppointmentDuration: Duration(minutes: 60)));
+  }
+
+  void onCalendarViewChange(String value) {
+    if (value == 'Day') {
+      _calendarView = CalendarView.day;
+    } else if (value == 'Week') {
+      _calendarView = CalendarView.week;
+    } else if (value == 'Work week') {
+      _calendarView = CalendarView.workWeek;
+    } else if (value == 'Month') {
+      _calendarView = CalendarView.month;
+    } else if (value == 'Timeline day') {
+      _calendarView = CalendarView.timelineDay;
+    } else if (value == 'Timeline week') {
+      _calendarView = CalendarView.timelineWeek;
+    } else if (value == 'Timeline work week') {
+      _calendarView = CalendarView.timelineWorkWeek;
+    }
+
+    setState(() {});
+  }
+
+  void onCalendarTapped(CalendarTapDetails calendarTapDetails) {
+    if (calendarTapDetails.targetElement != CalendarElement.calendarCell &&
+        calendarTapDetails.targetElement != CalendarElement.appointment) {
+      return;
+    }
+
+    setState(() {
+      _selectedAppointment = null;
+      _isAllDay = false;
+      _selectedColorIndex = 0;
+      _subject = '';
+      _notes = '';
+      if (_calendarView == CalendarView.month) {
+        _calendarView = CalendarView.day;
+      } else {
+        if (calendarTapDetails.appointments != null &&
+            calendarTapDetails.appointments!.length == 1) {
+          final Meeting meetingDetails = calendarTapDetails.appointments![0];
+          _startDate = meetingDetails.from;
+          _endDate = meetingDetails.to;
+          _isAllDay = meetingDetails.isAllDay;
+          _selectedColorIndex =
+              _colorCollection.indexOf(meetingDetails.background);
+          _subject = meetingDetails.eventName == '(No title)'
+              ? ''
+              : meetingDetails.eventName;
+          _notes = meetingDetails.description;
+          _selectedAppointment = meetingDetails;
+        } else {
+          final DateTime date = calendarTapDetails.date!;
+          _startDate = date;
+          _endDate = date.add(const Duration(hours: 1));
+        }
+        _startTime =
+            TimeOfDay(hour: _startDate.hour, minute: _startDate.minute);
+        _endTime = TimeOfDay(hour: _endDate.hour, minute: _endDate.minute);
+        Navigator.push<Widget>(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => const AppointmentEditor(),
+          ),
+        );
+      }
+    });
+  }
+
+  List<Meeting> getMeetingDetails() {
+    final List<Meeting> meetingCollection = <Meeting>[];
+    eventNameCollection = <String>[];
+    eventNameCollection.add('General Meeting');
+    eventNameCollection.add('Plan Execution');
+    eventNameCollection.add('Project Plan');
+    eventNameCollection.add('Consulting');
+    eventNameCollection.add('Support');
+    eventNameCollection.add('Development Meeting');
+    eventNameCollection.add('Scrum');
+    eventNameCollection.add('Project Completion');
+    eventNameCollection.add('Release updates');
+    eventNameCollection.add('Performance Check');
+
+    _colorCollection = <Color>[];
+    _colorCollection.add(const Color(0xFF0F8644));
+    _colorCollection.add(const Color(0xFF8B1FA9));
+    _colorCollection.add(const Color(0xFFD20100));
+    _colorCollection.add(const Color(0xFFFC571D));
+    _colorCollection.add(const Color(0xFF85461E));
+    _colorCollection.add(const Color(0xFFFF00FF));
+    _colorCollection.add(const Color(0xFF3D4FB5));
+    _colorCollection.add(const Color(0xFFE47C73));
+    _colorCollection.add(const Color(0xFF636363));
+
+    _colorNames = <String>[];
+    _colorNames.add('Green');
+    _colorNames.add('Purple');
+    _colorNames.add('Red');
+    _colorNames.add('Orange');
+    _colorNames.add('Caramel');
+    _colorNames.add('Magenta');
+    _colorNames.add('Blue');
+    _colorNames.add('Peach');
+    _colorNames.add('Gray');
+
+    _timeZoneCollection = <String>[];
+    _timeZoneCollection.add('Default Time');
+    _timeZoneCollection.add('Central European Standard Time');
+
+    final DateTime today = DateTime.now();
+    final Random random = Random();
+    for (int month = -1; month < 2; month++) {
+      for (int day = -5; day < 5; day++) {
+        for (int hour = 9; hour < 18; hour += 5) {
+          meetingCollection.add(Meeting(
+            from: today
+                .add(Duration(days: (month * 30) + day))
+                .add(Duration(hours: hour)),
+            to: today
+                .add(Duration(days: (month * 30) + day))
+                .add(Duration(hours: hour + 2)),
+            background: _colorCollection[random.nextInt(9)],
+            startTimeZone: '',
+            endTimeZone: '',
+            description: '',
+            isAllDay: false,
+            eventName: eventNameCollection[random.nextInt(7)],
+          ));
+        }
+      }
+    }
+
+    return meetingCollection;
+  }
+}
+
+class DataSource extends CalendarDataSource {
+  DataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  bool isAllDay(int index) => appointments![index].isAllDay;
+
+  @override
+  String getSubject(int index) => appointments![index].eventName;
+
+  @override
+  String getStartTimeZone(int index) => appointments![index].startTimeZone;
+
+  @override
+  String getNotes(int index) => appointments![index].description;
+
+  @override
+  String getEndTimeZone(int index) => appointments![index].endTimeZone;
+
+  @override
+  Color getColor(int index) => appointments![index].background;
+
+  @override
+  DateTime getStartTime(int index) => appointments![index].from;
+
+  @override
+  DateTime getEndTime(int index) => appointments![index].to;
+}
+
+class Meeting {
+  Meeting(
+      {required this.from,
+      required this.to,
+      this.background = Colors.green,
+      this.isAllDay = false,
+      this.eventName = '',
+      this.startTimeZone = '',
+      this.endTimeZone = '',
+      this.description = ''});
+
+  final String eventName;
+  final DateTime from;
+  final DateTime to;
+  final Color background;
+  final bool isAllDay;
+  final String startTimeZone;
+  final String endTimeZone;
+  final String description;
+}
